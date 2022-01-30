2c3f73e71a82cc249e34defad2f9bc9caa4c5096
Alapi <59810553+alapishah@users.noreply.github.com>
2022-01-30 14:15:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,12 @@ ext: 'png'
 }).addTo(map);
 
   // load GeoJSON from an external file
-  $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
-         var ratIcon = L.icon({
-    iconUrl: 'https://e7.pngegg.com/pngimages/170/541/png-clipart-robbery-burglary-crime-theft-thief-mammal-people.png',
-    iconSize: [30,30]
-  });    
-	  
-	  var crimes = L.geoJson(data,{
+   $.getJSON("https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson",function(data){
+    var ratIcon = L.icon({
+      iconUrl: 'https://e7.pngegg.com/pngimages/170/541/png-clipart-robbery-burglary-crime-theft-thief-mammal-people.png',
+      iconSize: [60,50]
+    });
+    var crimes = L.geoJson(data,{
       pointToLayer: function(feature,latlng){
         var marker = L.marker(latlng,{icon: ratIcon});
         marker.bindPopup(feature.properties.Location + '<br/>' + feature.properties.OPEN_DT);
@@ -25,7 +24,3 @@ ext: 'png'
     map.addLayer(clusters);
 });
 
-    var clusters = L.markerClusterGroup();
-    clusters.addLayer(ratIcon);
-    map.addLayer(clusters);
-});
